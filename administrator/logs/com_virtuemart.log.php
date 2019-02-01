93b92b18d3f286cce9d9e39b702e7cefdd5575c6
JoseVG1 <1530209@upv.edu.mx>
2019-01-31 21:33:29 -0600

    first commit

---
+++ administrator/logs/com_virtuemart.log.php
@@ -0,0 +1,5 @@
+#
+#<?php die("Forbidden."); ?>
+
+2019-01-31 15:43:09 ERROR vmError: Warning, the Safe Path is not configured yet. Use this link to the &lt;a href='http://localhost/portfolio2/administrator/index.php?option=com_virtuemart&amp;view=updatesmigration&amp;show_spwizard=1' &gt;setup wizard&lt;/a&gt;
+2019-01-31 15:43:20 ERROR vmError: Warning, the Safe Path is not configured yet. Use this link to the &lt;a href='http://localhost/portfolio2/administrator/index.php?option=com_virtuemart&amp;view=updatesmigration&amp;show_spwizard=1' &gt;setup wizard&lt;/a&gt;
